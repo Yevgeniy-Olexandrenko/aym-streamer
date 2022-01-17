b3d3b0786ddf615381f4a3a83c8b57f07fdcf610
Yevgeniy <66377737+Yevgeniy-Olexandrenko@users.noreply.github.com>
2022-01-17 17:34:21 +0200

    Added code for serial port communication

--- software/AYM_Streamer/src/AYM_Streamer.cpp
+++ software/AYM_Streamer/src/AYM_Streamer.cpp
@@ -1,20 +1,17 @@
-// AYM_Streamer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
+#include "SerialPort/SerialPort.h"
 
-int main()
+void main()
 {
-    std::cout << "Hello World!\n";
+	auto p1 = OpenPort(1);
+	auto p2 = OpenPort(1);
+
+	char sendstr[80] = "ACK";
+	char recivestr[80];
+
+	SendData(p1, sendstr);
+	ReciveData(p2, recivestr, 80);
+
+	ClosePort(p1);
+	ClosePort(p2);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

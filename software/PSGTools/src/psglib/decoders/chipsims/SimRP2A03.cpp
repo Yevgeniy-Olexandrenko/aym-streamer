11ea8f521da8ae26c5f4a2f2393953837fba6b18
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2022-09-15 23:46:42 +0300

    Refactoring and clean up

--- software/PSGTools/src/psglib/decoders/chipsims/SimRP2A03.cpp
+++ software/PSGTools/src/psglib/decoders/chipsims/SimRP2A03.cpp
@@ -126,22 +126,26 @@ void SimRP2A03::ConvertToSingleChip(const State& state, Frame& frame)
     // Triangle -> Envelope B
     if (state.triangle_enable)
     {
+        // enable triangle envelope
         uint16_t period = ConvertPeriod(state.triangle_period >> 3);
-
-        if (frame[0].GetData(E_Shape) != 0x0A) frame[0].Update(E_Shape, 0x0A);
+        if (frame[0].GetData(E_Shape) != 0x0A)
+        {
+            frame[0].Update(E_Shape, 0x0A);
+        }
         frame[0].UpdatePeriod(E_Period, period);
-        frame[0].Update(B_Volume, 0x10);
+        frame[0].Update(B_Volume, 0x10 + 0x08);
     }
     else
     {
-        frame[0].Update(B_Volume, 0x08);
+        // disable triangle envelope
+        uint8_t volumeB = frame[0].Read(B_Volume) & ~0x10;
+        frame[0].Update(B_Volume, volumeB);
     }
     
-    // Noise -> automatically chosen channel A or C
+    // Noise -> automatically chosen channel: A, B, C or A + C
     if (state.noise_enable)
     {
         uint16_t period = ConvertPeriod(state.noise_period >> 7);
-
         frame[0].UpdatePeriod(N_Period, period);
         DistributeNoiseBetweenChannels(state, frame, mixer);
     }
@@ -269,30 +273,35 @@ void SimRP2A03::ConvertToAY8930Chip(const State& state, Frame& frame)
     // Triangle -> Envelope B
     if (state.triangle_enable)
     {
-        uint16_t period = ConvertPeriod(state.triangle_period >> 3);
-
-        if (frame[0].GetData(EB_Shape) != 0x0A) frame[0].Update(EB_Shape, 0x0A);
-        frame[0].UpdatePeriod(EB_Period, period);
-        frame[0].Update(B_Volume, 0x20);
-
         // workaround for mixer
         EnableTone(mixer, Frame::Channel::B);
-        frame[0].UpdatePeriod(B_Period, 0x0000);
+        frame[0].UpdatePeriod(B_Period, 0x0);
         frame[0].Update(B_Duty, 0x08);
+
+        // enable triangle envelope
+        uint16_t period = ConvertPeriod(state.triangle_period >> 3);
+        if (frame[0].GetData(EB_Shape) != 0x0A)
+        {
+            frame[0].Update(EB_Shape, 0x0A);
+        }
+        frame[0].UpdatePeriod(EB_Period, period);
+        frame[0].Update(B_Volume, 0x20 + 0x10);
     }
     else
     {
-        frame[0].Update(B_Volume, 0x10);
+        // disable triangle envelope
+        uint8_t volumeB = frame[0].Read(B_Volume) & ~0x20;
+        frame[0].Update(B_Volume, volumeB);
     }
 
-    // Noise -> automatically chosen channel A or C
+    // Noise -> automatically chosen channel: A, B, C or A + C
     if (state.noise_enable)
     {
-        uint16_t period = ConvertPeriod(state.noise_period >> 4);
-
-        frame[0].UpdatePeriod(N_Period, period);
         frame[0].Update(N_AndMask, 0x0F);
         frame[0].Update(N_OrMask,  0x00);
+
+        uint16_t period = ConvertPeriod(state.noise_period >> 4);
+        frame[0].UpdatePeriod(N_Period, period);
         DistributeNoiseBetweenChannels(state, frame, mixer);
     }
 
@@ -305,7 +314,6 @@ void SimRP2A03::ConvertToAY8930Chip(const State& state, Frame& frame)
 void SimRP2A03::DistributeNoiseBetweenChannels(const State& state, Frame& frame, uint8_t& mixer)
 {
     int volumeN = ConvertVolume(state.noise_volume);
-    
     if (!state.triangle_enable)
     {
         EnableNoise(mixer, Frame::Channel::B);
@@ -316,22 +324,18 @@ void SimRP2A03::DistributeNoiseBetweenChannels(const State& state, Frame& frame,
         if (!state.pulse1_enable) frame[0].Update(A_Volume, volumeN);
         if (!state.pulse2_enable) frame[0].Update(C_Volume, volumeN);
 
-        auto volumeA = frame[0].Read(A_Volume);
-        auto volumeC = frame[0].Read(C_Volume);
-
         auto levelN = VolumeToLevel(volumeN);
-        auto levelA = VolumeToLevel(volumeA);
-        auto levelC = VolumeToLevel(volumeC);
+        auto levelA = VolumeToLevel(frame[0].Read(A_Volume));
+        auto levelC = VolumeToLevel(frame[0].Read(C_Volume));
 
         auto deltaA = std::abs(levelN - levelA);
         auto deltaC = std::abs(levelN - levelC);
         auto delta2 = std::abs(levelN - std::sqrtf(levelA * levelA + levelC * levelC));
         auto delta_ = std::min(std::min(deltaA, deltaC), delta2);
 
-        if (delta_ == deltaA)
-            EnableNoise(mixer, Frame::Channel::A);
-        else if (delta_ == deltaC)
-            EnableNoise(mixer, Frame::Channel::C);
+        if (delta_ == deltaA) EnableNoise(mixer, Frame::Channel::A);
+        else 
+        if (delta_ == deltaC) EnableNoise(mixer, Frame::Channel::C);
         else
         {
             EnableNoise(mixer, Frame::Channel::A);
@@ -344,7 +348,7 @@ void SimRP2A03::DistributeNoiseBetweenChannels(const State& state, Frame& frame,
 
 namespace
 {
-    const float c_volumeVsLevel[] =
+    const float c_volumeToLevel[] =
     {
         0.000f, 0.000f, 0.005f, 0.008f, 0.011f, 0.014f, 0.017f, 0.020f,
         0.024f, 0.030f, 0.035f, 0.040f, 0.049f, 0.058f, 0.068f, 0.078f,
@@ -359,23 +363,19 @@ float SimRP2A03::VolumeToLevel(uint8_t volume) const
     {
         volume <<= 1; ++volume;
     }
-    return c_volumeVsLevel[volume &= 0x1F];
+    return c_volumeToLevel[volume &= 0x1F];
 }
 
 uint8_t SimRP2A03::LevelToVolume(float level) const
 {
     level = std::max(0.f, std::min(1.f, level));
 
-    auto index = int(-1);
-    auto delta = float(1);
-    for (int i = 0; i < 32; ++i)
+    auto index = 0;
+    auto delta = std::abs(level - c_volumeToLevel[0]);
+    for (int i = 1; i < 32; ++i)
     {
-        auto d = std::abs(level - c_volumeVsLevel[i]);
-        if (d < delta)
-        {
-            delta = d;
-            index = i;
-        }
+        auto d = std::abs(level - c_volumeToLevel[i]);
+        if (d < delta) { delta = d; index = i; }
     }
 
     if (m_outputType != OutputType::AY8930Chip)

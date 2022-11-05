de145971c5b942895241a2c3561984c6ac2faf4c
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2022-11-05 12:21:28 +0200

    Preparing for the new firmware

--- firmware_new/firmware.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define HARDWARE_VERSION    211206
-#define BAUD_RATE_OUTPUT    9600
-#define BAUD_RATE_STREAM    57000
--- firmware_new/src/stream/uart/uart-stream.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "avr-support.h"
-#include "psg-access.h"
-
-void UARTStream_Open();
-void UARTStream_Update();
-void UARTStream_Close();

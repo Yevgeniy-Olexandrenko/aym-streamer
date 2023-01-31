ac23d837d720489629185d1792da5e09892eef15
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2023-01-31 15:24:13 +0200

    Added implementation for text exchange via uart along with binary data reading

--- firmware_new/src/uart-stream.cpp
+++ firmware_new/src/uart-stream.cpp
@@ -1,5 +1,6 @@
 #include "uart-stream.h"
 #include "psg-access.h"
+#include <stdlib.h>
 
 #include <avr/interrupt.h>
 #define  MU_TX_BUF 0
@@ -10,20 +11,29 @@
 // shared class instance
 uart_stream UARTStream;
 
-// hidden internals common to 
-// all instances of the class
+// shared references and objects
 static volatile SinglePSG* m_psg;
 static volatile MicroUART  m_uart;
+
+// handling input binary data
 static volatile uint8_t m_buf[64];
 static volatile uint8_t m_wrp;
 static volatile uint8_t m_rdp;
 static volatile uint8_t m_reg;
+
+// handling input strings
+static volatile uart_stream::Handler m_handler = nullptr;
+static volatile uint8_t m_str[64];
+static volatile uint8_t m_stp;
+
+// -----------------------------------------------------------------------------
 
 void uart_stream::Start(SinglePSG& psg)
 {
     // prepare internal state
     m_psg = &psg; m_reg = 0xFF;
     m_wrp = 0x00; m_rdp = 0x00;
+    m_stp = 0x00;
 
     // start UART for communication
     // and timer for PSG update
@@ -33,6 +43,11 @@ void uart_stream::Start(SinglePSG& psg)
     sei();
 }
 
+void uart_stream::SetInputHandler(Handler handler)
+{
+    m_handler = handler;
+}
+
 void uart_stream::Stop()
 {
     // stop PSG update timer and
@@ -40,6 +55,118 @@ void uart_stream::Stop()
     Timer1.stop();
     m_uart.end();
 }
+
+void uart_stream::Print(char chr, int num)
+{
+    while (num-- > 0) m_uart.write(chr);
+}
+
+void uart_stream::Print(const char* str)
+{
+    while (*str != '\0') m_uart.write(*str++);
+}
+
+void uart_stream::Print(const __FlashStringHelper* str)
+{
+    const char* p = (const char *)str;
+    while (char c = pgm_read_byte(p++)) m_uart.write(c);
+}
+
+void uart_stream::Print(uint8_t val, bool hex)
+{
+    if (hex)
+    {
+        PrintNibble(val >> 4);
+        PrintNibble(val);
+    }
+    else
+    {
+        PrintNumber(val);
+    }
+}
+
+void uart_stream::Print(uint16_t val, bool hex)
+{
+    if (hex)
+    {
+        Print(uint8_t(val >> 8));
+        Print(uint8_t(val));
+    }
+    else
+    {
+        PrintNumber(val);
+    }
+}
+
+void uart_stream::Print(uint32_t val, bool hex)
+{
+    if (hex)
+    {
+        Print(uint16_t(val >> 16));
+        Print(uint16_t(val));
+    }
+    else
+    {
+        PrintNumber(val);
+    }
+}
+
+void uart_stream::Println(char chr, int num = 1)
+{
+    Print(chr, num);
+    Println();
+}
+
+void uart_stream::Println(const char* str)
+{
+    Print(str);
+    Println();
+}
+
+void uart_stream::Println(const __FlashStringHelper* str)
+{
+    Print(str);
+    Println();
+}
+
+void uart_stream::Println(uint8_t val, bool hex)
+{
+    Print(val, hex);
+    Println();
+}
+
+void uart_stream::Println(uint16_t val, bool hex)
+{
+    Print(val, hex);
+    Println();
+}
+
+void uart_stream::Println(uint32_t val, bool hex)
+{
+    Print(val, hex);
+    Println();
+}
+
+void uart_stream::Println()
+{
+    m_uart.write('\r');
+    m_uart.write('\n');
+}
+
+void uart_stream::PrintNibble(uint8_t nibble)
+{
+    nibble &= 0x0F;
+    m_uart.write((nibble >= 0x0A ? 'A' - 0x0A : '0') + nibble);
+}
+
+void uart_stream::PrintNumber(int32_t number)
+{
+    char str[12];
+    ultoa(number, str, 10);
+    Print(str);
+}
+
+// -----------------------------------------------------------------------------
 
 void MU_serialEvent()
 {
@@ -67,15 +194,41 @@ ISR(TIMER1_A)
             m_psg->SetRegister(m_reg, data);
             m_reg = 0xFF;
         }
-        else if (data < 0x10)
+        else
         {
-            // handle register number
-            m_reg = data;
-        }
-        else if (data == 0xFF)
-        {
-            // handle frame begining
-            m_psg->Update();
+            // waiting for register number
+            if (data == 0xFF)
+            {
+                // handle frame begining
+                m_psg->Update();
+                m_stp = 0x00;
+            }
+            else if (data >= 0x20 && data <= 0x7F)
+            {
+                // handle new char for input string
+                if (m_stp < sizeof(m_str) - 2)
+                {
+                    m_str[m_stp++] = data;
+                }
+            }
+            else if (data < 0x10)
+            {
+                if (m_stp)
+                {
+                    // handle input string processing
+                    if (m_handler)
+                    {
+                        m_str[m_stp] = 0;
+                        m_handler(m_str);
+                    }
+                }
+                else
+                {
+                    // handle register number
+                    m_reg = data;
+                }
+                m_stp = 0x00;
+            }
         }
     }
 
--- firmware_new/src/uart-stream.h
+++ firmware_new/src/uart-stream.h
@@ -1,12 +1,41 @@
 #pragma once
 
+#include <avr/pgmspace.h>
+
+// support for strings in PROGMEM
+class __FlashStringHelper;
+#define FPSTR(pstr_pointer) (reinterpret_cast<const __FlashStringHelper *>(pstr_pointer))
+#define F(string_literal) (FPSTR(PSTR(string_literal)))
+
 class SinglePSG;
 
 class uart_stream
 {
 public:
+    using Handler = void (*)(const char* str);
+
     void Start(SinglePSG& psg);
+    void SetInputHandler(Handler handler);
     void Stop();
+
+    void Print(char chr, int num = 1);
+    void Print(const char* str);
+    void Print(const __FlashStringHelper* str);
+    void Print(uint8_t  val, bool hex = false);
+    void Print(uint16_t val, bool hex = false);
+    void Print(uint32_t val, bool hex = false);
+
+    void Println(char chr, int num = 1);
+    void Println(const char* str);
+    void Println(const __FlashStringHelper* str);
+    void Println(uint8_t  val, bool hex = false);
+    void Println(uint16_t val, bool hex = false);
+    void Println(uint32_t val, bool hex = false);
+    void Println();
+
+private:
+    void PrintNibble(uint8_t nibble);
+    void PrintNumber(int32_t number);
 };
 
 extern uart_stream UARTStream;
